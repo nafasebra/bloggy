5b82346259259361a1e4e55bb918e4a35d718cfa
Nafas Ebrahimi <ebranafas@gmail.com>
2025-08-01 23:06:35 +0330

    backend(fit): handle swagger and add docs to auth

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -3,17 +3,25 @@ import { Post,Body } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('register')
+    @ApiOperation({ summary: 'Register a new user' })
+    @ApiResponse({ status: 201, description: 'User registered successfully' })
+    @ApiBody({ type: RegisterDto })
     register(@Body() registerDto: RegisterDto) {
         return this.authService.register(registerDto);
     }
 
     @Post('login')
+    @ApiOperation({ summary: 'Login a user' })
+    @ApiResponse({ status: 200, description: 'User logged in successfully' })
+    @ApiBody({ type: LoginDto })
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
     }
