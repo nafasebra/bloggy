cf687ff1688e8b3460e86461c9ec29fe6d145b59
Nafas Ebrahimi <ebranafas@gmail.com>
2025-09-29 22:43:14 +0330

    fix(front): fix links

--- front/src/components/pages/home/latest-users.tsx
+++ front/src/components/pages/home/latest-users.tsx
@@ -109,26 +109,6 @@ export default async function LatestUsers() {
             </div>
           ))}
         </div>
-
-        <div className="text-center mt-12">
-          <Link
-            href="/blog"
-            className="inline-flex items-center px-6 py-3 border border-gray-300 dark:border-gray-600 text-base font-medium rounded-lg text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors duration-200"
-          >
-            Discover More Writers
-            <svg
-              className="ml-2 -mr-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </Link>
-        </div>
       </div>
     </section>
   );
